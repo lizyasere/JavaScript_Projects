7901b0731b54ca22977f8d1119927ebe949e06bd
lizyasere <iyasere.elizabeth@gmail.com>
2023-05-11 10:47:28 +0100

    user-defined

--- variables.js
+++ variables.js
@@ -1,6 +1,16 @@
 //A JavaScript exercise to create a variable using a user-defined name.  
 
-var FirstName = "Akinwale";
-console.log(FirstName)
-FirstName = "Elizabeth";
-console.log(FirstName)
+let var_name = "newVariabe";
+let var_value = "newValue";
+
+this[var_name] = var_value;
+console.log(var_name);
+console.log(this[var_name]);
+
+
+
+
+// var FirstName = "Akinwale";
+// console.log(FirstName)
+// FirstName = "Elizabeth";
+// console.log(FirstName)
